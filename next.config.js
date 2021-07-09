3abc22fe1ed19a6d4fb33214d3bc4ea2a5d90202
Jakub Szpil <kuba0122@gmail.com>
2021-07-09 21:59:59 +0200

    Migrating to Typescript and adding some dev dependencies

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+const path = require('path');
+
 module.exports = {
+  webpack: (config) => {
+    config.resolve.modules.push(path.resolve('./src/'));
+
+    return config;
+  },
   reactStrictMode: true,
-}
+};
